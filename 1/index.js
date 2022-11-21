cb9fa1d4f47e043f044676fae776a8565b3f1fd4
Alex <lesnar1@mail.ru>
2022-11-21 21:49:47 +0300

    lesson_1_4/5

--- 1/index.js
+++ 1/index.js
@@ -1,7 +1,48 @@
-import colors from 'colors'
+import colors from "colors";
 
-const from = process.argv[2];
-const to = rocess.argv[3];
+const Colors = { GREEN: 0, YELLOW: 1, RED: 2}
+let currentColor = Colors.GREEN;
+const from = parseInt(process.argv[2]);
+const to = parseInt(process.argv[3]);
+let noPrime = true;
 
+if (isNaN(from) || isNaN(to)) {
+    console.log('Некорректный ввод. Введите целые числа!'.red);
+}
+const isPrime = (num) => {
+    if (num <=1)
+    return false;
+    for (let i = 2; i < num; i++)
+    if (num % i === 0) return false;
+    return true;
+}
+
+const changeColor = () => {
+        currentColor++;
+        if (currentColor > Colors.RED)
+            currentColor = Colors.GREEN;
+    }
+    
+    const colorPrint = (num) => {
+        if(noPrime) noPrime = false;
+        switch (currentColor){
+            case Colors.RED:
+                console.log(`${num}`.red);
+                break;
+            case Colors.GREEN:
+                console.log(`${num}`.green);
+                break;
+            case Colors.YELLOW:
+                console.log(`${num}`.yellow);
+                break;
+        }
+        changeColor();
+    }
+
+for (let i = from; i <= to; i++){
+    if (isPrime(i)) colorPrint(i);
+    }
+    if(noPrime)
+    console.log(`Некорректный ввод. Введите целые числа![${from},${to}]`.red);
 
 
