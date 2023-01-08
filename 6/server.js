18dada587e1802dfdb2f28ed7cf30dd134403356
Alex <lesnar1@mail.ru>
2023-01-08 22:18:24 +0300

    lesson_6_2/10_err

--- 6/server.js
+++ 6/server.js
@@ -7,10 +7,22 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, './public')));
 
 io.on('connection', (socket) => {
     console.log('New Connection...');
+
+    socket.emit('message', 'Welcome to Chat!');
+
+    socket.broadcast.emit('message', 'A new user joined...');
+
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'User has left chat');
+    });
+
+
+
 });
 
 const PORT = 3001 || process.env.PORT;
